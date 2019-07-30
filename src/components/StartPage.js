66c5e683ee29e52c498c5c7ec850007099752a71
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-07-30 11:17:27 +0200

    Initialize the files and folders

---
+++ src/components/StartPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class StartPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1> HOI </h1>
+            </div>
+        )
+    }
+}
+
+export default StartPage
